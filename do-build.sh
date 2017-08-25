d648d9c7675a01e80fd262ca0cb7168435dcb19f
Benjamin Rajs Wagner <brajswagner@gmail.com>
2017-08-25 19:39:15 -0400

    More changes to build

--- do-build.sh
+++ do-build.sh
@@ -10,5 +10,10 @@ git commit -m "$*" && \
 LAST_COMMIT=`git rev-parse HEAD` && \
 git push origin sources && \
 bundle exec jekyll build && \
-(cd _site/ && touch .nojekyll && git checkout master && git add -A && git commit -m "Build of master/$LAST_COMMIT" && git push origin master) && \
+(cd _site/ && \
+ touch .nojekyll && \
+ git checkout master && \
+ git add -A && \
+ git commit -m "Build of master/$LAST_COMMIT" && \
+ git push origin master)
 git checkout sources
