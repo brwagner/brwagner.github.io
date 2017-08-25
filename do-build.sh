53c5f96ac812fa1159f7b0e32d272d232644b79d
Benjamin Rajs Wagner <brajswagner@gmail.com>
2017-08-25 18:34:11 -0400

    Trying something out

--- do-build.sh
+++ do-build.sh
@@ -3,7 +3,4 @@
 # Build script for things
 
 git commit -m "$*" && \
-LAST_COMMIT=`git rev-parse HEAD` && \
-git push origin soruces && \
-bundle exec jekyll build && \
-(cd _site/ && touch .nojekyll && git commit -m "Build of master/$LAST_COMMIT" && git push origin master)
+LAST_COMMIT=`git rev-parse HEAD`
