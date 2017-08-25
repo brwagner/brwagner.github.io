751b9d108f5362aa97b6dc151021c6a9635237ff
Benjamin Rajs Wagner <brajswagner@gmail.com>
2017-08-25 18:36:36 -0400

    Turns out it was a spelling error

--- do-build.sh
+++ do-build.sh
@@ -3,4 +3,7 @@
 # Build script for things
 
 git commit -m "$*" && \
-LAST_COMMIT=`git rev-parse HEAD`
+LAST_COMMIT=`git rev-parse HEAD` && \
+git push origin sources && \
+bundle exec jekyll build && \
+(cd _site/ && touch .nojekyll && git commit -m "Build of master/$LAST_COMMIT" && git push origin master)
