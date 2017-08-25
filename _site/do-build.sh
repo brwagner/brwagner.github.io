67f141d6c2c52ee08afb4bcc7eb781d6a0f468a5
Benjamin Rajs Wagner <brajswagner@gmail.com>
2017-08-25 19:39:20 -0400

    Build of master/d648d9c7675a01e80fd262ca0cb7168435dcb19f

---
+++ _site/do-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Build script which keeps master in sync with sources
+
+# Github's version of Jekyll doens't like my plugins so what we're doing is
+# making a new build and then pushing code to master everytime we make a
+# change to sources
+
+git commit -m "$*" && \
+LAST_COMMIT=`git rev-parse HEAD` && \
+git push origin sources && \
+bundle exec jekyll build && \
+(cd _site/ && \
+ touch .nojekyll && \
+ git checkout master && \
+ git add -A && \
+ git commit -m "Build of master/$LAST_COMMIT" && \
+ git push origin master)
+git checkout sources
