8e53c5781eb0252baa3a1620b2b5a3678078a055
Benjamin Rajs Wagner <brajswagner@gmail.com>
2017-08-25 18:22:38 -0400

    ""

---
+++ do-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git commit -m \""$@"\" && \
+LAST_COMMIT=`git rev-parse HEAD` && \
+git push origin master && \
+bundle exec jekyll build && \
+cd _site/ && git commit -m "Build of master/$LAST_COMMIT"
+
