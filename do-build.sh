927556b4234bef1b7c9421394252cece22e69708
Benjamin Rajs Wagner <brajswagner@gmail.com>
2017-08-25 18:24:54 -0400

    "Test build script"

--- do-build.sh
+++ do-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git commit -m \""$@"\" && \
+git commit -m \"$@\" && \
 LAST_COMMIT=`git rev-parse HEAD` && \
 git push origin master && \
 bundle exec jekyll build && \
-cd _site/ && git commit -m "Build of master/$LAST_COMMIT"
+cd _site/ && touch .nojekyll && git commit -m "Build of master/$LAST_COMMIT"
 
